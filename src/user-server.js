6aca9624148087966e499309286b1b7aba4c2a4b
HopeKiddo <maglaquijohnhopefuedan@gmail.com>
2025-01-29 21:50:45 +0800

    asdasd

--- src/user-server.js
+++ src/user-server.js
@@ -1623,7 +1623,7 @@ app.post('/xera/v1/api/user/mainnet/mintnft/sol', authenticateToken, async (req,
             `INSERT INTO xera_asset_nfts 
             (nft_id, nft_collection, nft_version, nft_icon, nft_name, nft_content, nft_creator, nft_owner, nft_type, nft_status, nft_rarity, nft_parts, nft_stakeable, nft_redeemable, nft_price, nft_token, nft_token_id, nft_info)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-            [nft_id, nft_collection, nft_version, nft_icon, nft_name, nft_content, nft_creator, nft_creator, nft_type, nft_status, nft_rarity, "[]", true, true, 0, '', '', nft_info]
+            [nft_id, nft_collection, nft_version, nft_icon, nft_name, nft_content, nft_creator, nft_creator, nft_type, nft_status, nft_rarity, "[]", 'true', 'true', 0, '', '', nft_info]
         );
 
         if (addNft.affectedRows === 0) {
