7d5969198226bfe36f44f07e0c20cfb87903b78c
HopeKiddo <maglaquijohnhopefuedan@gmail.com>
2025-02-04 17:15:18 +0800

    asdasdasd

--- src/user-server.js
+++ src/user-server.js
@@ -1050,6 +1050,49 @@ app.post('/xera/v1/api/user/security', authenticateToken, async (req, res) => {
     }
 });
 
+app.post('/xera/v1/api/user/last-transaction', authenticateToken, async (req, res) => {
+    const { user } = req.body;
+
+    if (!user) {
+        return res.json({ success: false, message: 'No address provided' });
+    }
+    let transactionData = [];
+    try {
+        const [getUserlastTransactionMainnet] = await db.query('SELECT transaction_hash, transaction_date FROM xera_mainnet_transactions WHERE sender_address = ? OR receiver_address = ? ORDER BY transaction_date DESC LIMIT 1', [user]);
+        
+        if (getUserlastTransactionMainnet.length > 0) {
+            // Clean response by removing sensitive data like id, ip_address, etc.
+            const cleantransactiondata = getUserlastTransactionMainnet.map(({ transaction_block, transaction_origin, sender_address, receiver_address, transaction_command, transaction_amount, transaction_token, transaction_token_id, transaction_fee_amount, transaction_fee_token, transaction_fee_token_id, transaction_validator, transaction_info, ...clean }) => clean);
+            const latestTransaction = {
+                MainnetTransactionHash: cleantransactiondata[0].transaction_hash,
+                MainnetTransactionDate: cleantransactiondata[0].transaction_date
+            }
+            transactionData.push(...latestTransaction);
+        } else {
+            return res.json({ success: false, message: "No security data found" });
+        }
+
+        const [getUserlastTransaction] = await db.query('SELECT transaction_hash, transaction_date FROM xera_network_transactions WHERE sender_address = ? OR receiver_address = ? ORDER BY transaction_date DESC LIMIT 1', [user]);
+        
+        if (getUserlastTransaction.length > 0) {
+            // Clean response by removing sensitive data like id, ip_address, etc.
+            const cleantransactiondata = getUserlastTransaction.map(({ transaction_block, transaction_origin, sender_address, receiver_address, transaction_command, transaction_amount, transaction_token, transaction_token_id, transaction_fee_amount, transaction_fee_token, transaction_fee_token_id, transaction_validator, transaction_info, ...clean }) => clean);
+            
+            const latestTransaction = {
+                NetworkTransactionHash: cleantransactiondata[0].transaction_hash,
+                NetworkTransactionDate: cleantransactiondata[0].transaction_date
+            }
+            transactionData.push(...latestTransaction);
+        } else {
+            return res.json({ success: false, message: "No security data found" });
+        }
+
+        return res.json({ success: true, message: `Successfully retrieved transaction datas. Wallet: ${user}`, transactions: transactionData });
+    } catch (error) {
+        return res.json({ success: false, message: 'Error retrieving security data', error: error.message });
+    }
+});
+
 // Telegram Task Endpoint
 app.post('/xera/v1/api/user/task/telegram', authenticateToken, async (req, res) => {
     const { data } = req.body;
