e22fd4c62a2fe954fcaca4e8b9aa833c0f2de5b3
HopeKiddo <maglaquijohnhopefuedan@gmail.com>
2025-02-04 17:50:02 +0800

    asdasd

--- src/proxy.js
+++ src/proxy.js
@@ -237,7 +237,7 @@ app.post('/xera/v1/api/public/user', async (req, res) => {
 
 try {
 
-  const [security, transactions, followers, balance, nodes, nfts] = await Promise.all([
+  const [security, transactions,lastTransactions, followers, balance, nodes, nfts] = await Promise.all([
     axios.post(`${xeraBaseAPI}/user/security`, {user: user.address}, { headers }),
     axios.post(`${xeraBaseAPI}/user/last-transaction`, {user: user.address}, { headers }),
     axios.post(`${xeraBaseAPI}/user/transactions`, {user: user.address}, { headers }),
@@ -250,6 +250,7 @@ try {
   const allData = {
       security: security.data || {},
       transactions: transactions.data || {},
+      lastTransactions: lastTransactions.data || {},
       followers: followers.data || {},
       balance: balance.data || {},
       nodes: nodes.data || {},
